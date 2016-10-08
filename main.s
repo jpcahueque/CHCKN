20e16cf7250cecd2f1a7e84de50a76f4b7dadb4c
jpcahueque <jpcahueque@gmail.com>
2016-10-08 15:11:29 -0600

    Update main.s

--- main.s
+++ main.s
@@ -10,6 +10,8 @@
 .global main
 
 main:	
+	/*Cargar el vector de turnos de gallinas*/
+	ldr r10,=vectorRandom
 fase1:
 
 	ldr r0, =main_menuWidth
@@ -19,6 +21,8 @@ fase1:
 	ldr r2, =main_menu	
 	bl draw_image
 
+	ldr r11,=delayButton
+	ldr r11,[r11]
 	bl delay
 
 	bl GetGpioAddress
@@ -63,6 +67,8 @@ instrct:
 	ldr r2, =instructions	
 	bl draw_image
 	
+	ldr r11,=delayButton
+	ldr r11,[r11]	
 	bl delay 
 	mov r0,#16
 	bl GetGpio
@@ -71,17 +77,19 @@ instrct:
 	beq fase1
 	b instrct
 play:
-	
+	/*Dibujar el paisaje de fondo una sola vez*/	
 	ldr r0, =game_landscapeWidth
 	ldr r0, [r0]
 	ldr r1, =game_landscapeHeight
 	ldr r1, [r1]
 	ldr r2, =game_landscape
 	bl draw_image
+	/*Luego verificar a que gallina le toca pasar*/
+	b verificar
 
 	mov r4, #1
 
-
+	/*Si le toca a la gallna verde*/
 	green_chicken_moving:
 	/*step del mask*/
 	mov r0,#75
@@ -112,19 +120,37 @@ play:
 
 	cmp r4, #3
 	movgt r4, #1
-
+	/*Caminar hasta que llegue a x_limit*/
 	ldr r5, =x_checkpoint
 	ldr r6, [r5]
+	ldr r0,=x_limit
+	ldr r0,[r0]
+	cmp r6,r0
+	/*Si llega al limite, verificar el turno*/
+	/*Hacer el masking aqui*/
+	bge verificar
 	add r6, #10
 	str r6, [r5]
+
+	ldr r11,=delayL1
+	ldr r11,[r11]
 	bl delay
-
-	
-
-	
 
 	b green_chicken_moving
 
+	/*r10 contiene el address del siguiente turno*/
+	verificar:
+	/*Resetear x_checkpoint a 0 para la siguiente*/
+	ldr r0,=x_checkpoint
+	ldr r11,[r0]
+	mov r11,#0
+	str r11,[r0]
+	/*Comparar el turno y ejecutarlo*/
+	ldr r11,[r10],#4
+	cmp r11,#1
+	beq green_chicken_moving
+	//cmp r11,#2
+	//beq blue_chicken_moving	
 	
 	ldr r0, =msg
 	bl puts
@@ -138,6 +164,9 @@ end:
 /* Finaliza el programa */	
 mov r7,#1
 swi 0 
+@----------------------------
+@Verificar turno de gallina
+@NO input--------------------
 
 @ ---------------------------
 @ Delay function
@@ -146,13 +175,11 @@ swi 0
 delay:
     push {r7}
     mov r7,#0
-    ldr r0,=delayReg
-    ldr r0,[r0]
     b compare
 loop:
     add r7,#1     //r7++
 compare:
-    cmp r7,r0     //test r7 == r11
+    cmp r7,r11     //test r7 == r11
     bne loop
     pop {r7}
     mov pc, lr
@@ -166,9 +193,11 @@ x: .word 0
 y: .word 50
 
 /* Constantes */
-delayReg:.word 90000000
-
-x_limit: .word 1360
+delayL1:.word 10000000
+vectorRandom: .word 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+delayButton:.word 90000000
+x_limit: .word 650
 .global myloc
 myloc: .word 0
 msg: .asciz "***************** DEBUG ********************"
+
